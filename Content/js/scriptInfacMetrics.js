65558260ae918cbafcbcca1d7193f0215bdb419b
CristhianZc <58814384+CristhianZc@users.noreply.github.com>
2024-04-10 23:28:32 -0500

    js

---
+++ Content/js/scriptInfacMetrics.js
@@ -0,0 +1,14 @@
+﻿(function ($) {
+    var $body = $('body');
+    var $loader = $('.dt-loader-container');
+
+    if ($loader.length) {
+        $loader.delay(300).fadeOut('noraml', function () {
+            $body.css('overflow', 'auto');
+            $root.css('opacity', '1');
+            $(document).trigger('loader-hide');
+        });
+    } else {
+        $(document).trigger('loader-hide');
+    }
+})(jQuery);
